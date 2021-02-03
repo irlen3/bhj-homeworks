d96bf3c3aaf2c96e944703d8273143a578485dd3
Irina Koval <irlen3@gmail.com>
2021-02-03 14:56:29 +0100

    second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,17 +6,20 @@ for (const link of menuLinks){
 
 // Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
 function openMenu(event) {  
+    // проверка, если был клик на открытом меню, то просто закрыть его и завершить выполнение обработчика события.
+    let menuClosest = event.target.nextElementSibling;
+    if(menuClosest.classList.contains('menu_active')) {
+        menuClosest.classList.remove('menu_active');
+        return;
+    } 
+
     let menuSubs = document.querySelectorAll('.menu_sub');
     for (i=0; i < menuSubs.length; i++) {
         if(menuSubs[i].classList.contains('menu_active'))
-            menuSubs[i].classList.remove('menu_active');
+            menuSubs[i].classList.remove('menu_active');      
     }
 
-    let currentMenu = this.closest('.menu__link');
-    let menuClosest = currentMenu.nextElementSibling;   
-    if(menuClosest) {
-        menuClosest.classList.toggle('menu_active');
-    }
+    menuClosest.classList.add('menu_active');
 
     // Запрещайте переход по ссылке для тех, что имеют вложенное меню. Остальные пункты меню 
     // должны без помех переводить пользователя на соответствующие страницы.
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,4 +1,4 @@
-let sliderPrev, sliderNext, numberSlides, slides;
+let sliderPrev, sliderNext, numberSlides, slides, mas;
 
 // Установите обработчики события click на элементах «Влево» и «Вправо»
 sliderPrev = document.querySelector('.slider__arrow_prev');
@@ -7,29 +7,22 @@ sliderPrev.addEventListener('click', imagePrev);
 sliderNext.addEventListener('click', imageNext);
 
 slides = document.querySelectorAll('.slider__item');
+mas = Array.from(slides);
+
 numberSlides = slides.length;
-let currentSlide = 0;
+let index = 0;
 
 // управление слайдером
-function imagePrev() {
-  for (currentSlide; currentSlide < numberSlides; ) {
-    if(slides[currentSlide].classList.contains('slider__item_active'))
-      slides[currentSlide].classList.remove('slider__item_active');
-      if(currentSlide === 0)
-        currentSlide = numberSlides - 1;
-      else 
-      currentSlide--;
-      slides[currentSlide].classList.add('slider__item_active');
-      return currentSlide;
-    }
+function imagePrev(event) {
+  index = mas.findIndex(element => element.classList.contains('slider__item_active'));
+  slides[index].classList.remove('slider__item_active');
+  index = (index === 0) ? (numberSlides - 1) : index - 1;
+  slides[index].classList.add('slider__item_active');
 }
 
-function imageNext() { 
-  for (currentSlide; currentSlide < numberSlides; ) {
-    if(slides[currentSlide].classList.contains('slider__item_active'))
-      slides[currentSlide].classList.remove('slider__item_active');
-      currentSlide = (currentSlide + 1) % numberSlides;  //increment the counter
-      slides[currentSlide].classList.add('slider__item_active');
-      return currentSlide;
-    }
+function imageNext() {
+  index = mas.findIndex(element => element.classList.contains('slider__item_active'));
+  slides[index].classList.remove('slider__item_active');
+  index = (index + 1) % numberSlides;  //increment the counter
+  slides[index].classList.add('slider__item_active');
 }
