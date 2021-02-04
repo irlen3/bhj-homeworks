e411a114772092a2bc2ce9aafd0c16f258cf324b
Irina Koval <irlen3@gmail.com>
2021-02-04 14:50:53 +0100

    third commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,27 +6,24 @@ for (const link of menuLinks){
 
 // Найдите меню рядом со ссылкой. Если оно есть, переключите у него класс menu_active
 function openMenu(event) {  
-    // проверка, если был клик на открытом меню, то просто закрыть его и завершить выполнение обработчика события.
     let menuClosest = event.target.nextElementSibling;
     if(menuClosest.classList.contains('menu_active')) {
         menuClosest.classList.remove('menu_active');
-        return;
-    } 
+        event.preventDefault();
+    } else {
+        let menuSubs = document.querySelectorAll('.menu_sub');
+        for (i=0; i < menuSubs.length; i++) {
+            if(menuSubs[i].classList.contains('menu_active'))
+                menuSubs[i].classList.remove('menu_active');      
+        }
+        menuClosest.classList.add('menu_active');
 
-    let menuSubs = document.querySelectorAll('.menu_sub');
-    for (i=0; i < menuSubs.length; i++) {
-        if(menuSubs[i].classList.contains('menu_active'))
-            menuSubs[i].classList.remove('menu_active');      
+// Запрещайте переход по ссылке для тех, что имеют вложенное меню.  
+// Остальные пункты меню4должны без помех переводить пользователя на соответствующие страницы.
+        if(!menuClosest.nextElementSibling) {
+            event.preventDefault();
+        }  
     }
-
-    menuClosest.classList.add('menu_active');
-
-    // Запрещайте переход по ссылке для тех, что имеют вложенное меню. Остальные пункты меню 
-    // должны без помех переводить пользователя на соответствующие страницы.
-    if(!menuClosest.nextElementSibling) {
-       event.preventDefault();
-       //return false;
-    }  
 }
 
 
